11602bdda494e96e32735536a034bf791644ebc8
Koushik <koushik.nov01@gmail.com>
2021-08-11 19:35:18 +0530

    update setup file

--- setup.sh
+++ setup.sh
@@ -1,13 +1,16 @@
-mkdir -p ~/.streamlit/
-echo "
-[general]n
-email = "koushik.nov01@gmail.com"n
-" > ~/.streamlit/credentials.toml
-echo "
-[server]n
-headless = truen
-enableCORS=falsen
-port = $PORTn
-" > ~/.streamlit/config.toml
 pip install torch==1.8.1+cpu -f https://download.pytorch.org/whl/torch_stable.html
 pip install torchvision==0.9.1+cpu -f https://download.pytorch.org/whl/torch_stable.html
+
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"koushik.nov01@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
